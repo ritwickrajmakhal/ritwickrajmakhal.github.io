6dfed15b9856d065e0d254e23f875463ab02c525
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2023-08-05 05:42:00 +0530

    go to top button, contact page added

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import Home from './components/Home';
 import Portfolio from './components/Portfolio';
 import About from './components/About';
 import Footer from './components/Footer';
+import Contact from './components/Contact';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   return (
     <div className="App">
+      <a href='#home'><i className="fa-solid fa-arrow-up position-fixed p-2 text-light fs-5 m-3 rounded-circle border bottom-0 end-0"></i></a>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} nav={data.website.nav} />
       <Home darkMode={darkMode} home={{ name: data.website.user.name, socialHandles: data.website.user.socialHandles}} />
       <Portfolio darkMode={darkMode} portfolios={data.website.portfolios} />
       <About darkMode={darkMode} about={data.website.user}/>
+      <Contact darkMode={darkMode} />
       <Footer darkMode={darkMode} footer={data.website.user}/>
     </div>
   );
---
+++ src/components/Contact.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useForm, ValidationError } from '@formspree/react';
+export default function Contact(props) {
+    const [state, handleSubmit] = useForm("mdoravon");
+    return (
+        <div id='contact' className={props.darkMode ? 'dark text-bg-dark' : 'light text-bg-light'}>
+            <div className="container py-3">
+                <h1 className='text-center'>Contact</h1>
+                {state.succeeded ?
+                    <div className="alert alert-warning alert-dismissible fade show mb-3" role="alert">
+                        Thank you for reaching out! I'll get back to you shortly.
+                        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    :
+                    <form onSubmit={handleSubmit}>
+                        <input name="subject" value="Portfolio Website Contact" hidden disabled />
+                        <div className="mb-3">
+                            <label htmlFor="exampleFormControlInput1" className="form-label">Name</label>
+                            <input type="text" name="name" className="form-control" id="exampleFormControlInput1" placeholder="Ritwick Raj Makhal" />
+                            <ValidationError prefix="Name" field="name" errors={state.errors} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                            <input type="email" name="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                            <ValidationError prefix="Email" field="email" errors={state.errors} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                            <textarea name="message" className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <ValidationError prefix="Message" field="message" errors={state.errors} />
+                        </div>
+                        <div className="d-grid gap-2 col-6 mx-auto">
+                            <button className='btn btn-outline-light' type="submit" disabled={state.submitting}>Submit</button>
+                        </div>
+                    </form>
+                }
+            </div>
+        </div>
+    )
+}
